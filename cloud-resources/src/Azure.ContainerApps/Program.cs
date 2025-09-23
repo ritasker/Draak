dd48ec586db8d686b723ed86b30a75ad053c312f
Richard Tasker <ritasker8t2@gmail.com>
2025-09-23 20:22:38 +0100

    Add IaC for AZ Container Apps Environment (#4)
    * Add container apps CD workflow and infrastructure setup
    
    - Introduced a new GitHub Actions workflow for container apps CD.
    - Defined Pulumi infrastructure for managing Azure resources, including resource groups, log workspaces, and managed environments.
    - Updated configuration for production environment.
    
    * typos
    
    * Add todo
    
    * move pulumi config to the correct file
    
    * add log retension config

--- cloud-resources/src/Azure.ContainerApps/Program.cs
+++ cloud-resources/src/Azure.ContainerApps/Program.cs
@@ -1,15 +1,78 @@
-﻿using Pulumi.AzureNative.Resources;
+﻿using System;
 using System.Collections.Generic;
 using Pulumi;
+using Pulumi.AzureNative.App;
+using Pulumi.AzureNative.App.Inputs;
+using Pulumi.AzureNative.OperationalInsights.Inputs;
+using Pulumi.AzureNative.Resources;
+using Insights = Pulumi.AzureNative.OperationalInsights;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
     var stackName = Pulumi.Deployment.Instance.StackName;
-    var config = new Config();
-    var location = config.Require("location");
-    var resourceGroup = new ResourceGroup("resourceGroup");
+    var resourceGroupName = $"container-apps-{stackName}";
+    
+    var config = new Config("container-apps");
+    var environment = config.Require("environment");
+
+    var resourceGroup = new ResourceGroup("resourceGroup", new ResourceGroupArgs
+    {
+        ResourceGroupName = resourceGroupName,
+        Tags = {{ "environment", environment}}
+    });
+    
+    var workspaceName = $"{environment}-logs";
+    var workspace = new Insights.Workspace(workspaceName, new Insights.WorkspaceArgs
+    {
+        WorkspaceName     = workspaceName,
+        ResourceGroupName = resourceGroup.Name,
+        Sku               = new WorkspaceSkuArgs { Name = "PerGB2018" },
+        RetentionInDays   = config.RequireInt32("logs-retention-days"),
+        Tags              = { { "environment", environment } }
+    });
+    
+    var sharedKeys = Output.Tuple(resourceGroup.Name, workspace.Name)
+        .Apply(items => Insights.GetSharedKeys.InvokeAsync(
+            new Insights.GetSharedKeysArgs
+            {
+                ResourceGroupName = items.Item1,
+                WorkspaceName     = items.Item2
+            }));
+    
+    // TODO remove this comment
+    
+    var managedEnvironment = new ManagedEnvironment(
+        environment,
+        new ManagedEnvironmentArgs
+        {
+            EnvironmentName   = environment,
+            ResourceGroupName = resourceGroup.Name,
+            ZoneRedundant     = false,
+            AppLogsConfiguration = new AppLogsConfigurationArgs
+            {
+                Destination = "log-analytics",
+                LogAnalyticsConfiguration = new LogAnalyticsConfigurationArgs
+                {
+                    CustomerId = workspace.CustomerId,
+                    SharedKey  = sharedKeys.Apply(r => r.PrimarySharedKey)!
+                }
+            },
+            Tags = { { "environment", environment } }
+        },
+        new CustomResourceOptions
+        {
+            CustomTimeouts = new CustomTimeouts
+            {
+                Create = TimeSpan.FromMinutes(5),
+                Update = TimeSpan.FromMinutes(5),
+                Delete = TimeSpan.FromMinutes(5)
+            }
+        });
 
     return new Dictionary<string, object?>
     {
+        ["Id"]        = managedEnvironment.Id,
+        ["Name"]      = managedEnvironment.Name,
+        ["StaticIp "] = managedEnvironment.StaticIp
     };
 });
