98f449d691eb94ba26e312c91a23adc763190f0d
Richard Tasker <ritasker8t2@gmail.com>
2025-09-27 15:35:14 +0100

    revert changes to the container registry and container app pulumi.

--- cloud-resources/src/Azure.ContainerApps/Program.cs
+++ cloud-resources/src/Azure.ContainerApps/Program.cs
@@ -11,48 +11,50 @@ return await Pulumi.Deployment.RunAsync(() =>
 {
     var stackName = Pulumi.Deployment.Instance.StackName;
     var resourceGroupName = $"container-apps-{stackName}";
-
+    
     var config = new Config("container-apps");
     var environment = config.Require("environment");
 
     var resourceGroup = new ResourceGroup("resourceGroup", new ResourceGroupArgs
     {
         ResourceGroupName = resourceGroupName,
-        Tags = { { "environment", environment } }
+        Tags = {{ "environment", environment}}
     });
-
+    
     var workspaceName = $"{environment}-logs";
     var workspace = new Insights.Workspace(workspaceName, new Insights.WorkspaceArgs
     {
-        WorkspaceName = workspaceName,
+        WorkspaceName     = workspaceName,
         ResourceGroupName = resourceGroup.Name,
-        Sku = new WorkspaceSkuArgs { Name = "PerGB2018" },
-        RetentionInDays = config.RequireInt32("logs-retention-days"),
-        Tags = { { "environment", environment } }
+        Sku               = new WorkspaceSkuArgs { Name = "PerGB2018" },
+        RetentionInDays   = config.RequireInt32("logs-retention-days"),
+        Tags              = { { "environment", environment } }
     });
-
+    
     var sharedKeys = Output.Tuple(resourceGroup.Name, workspace.Name)
         .Apply(items => Insights.GetSharedKeys.InvokeAsync(
             new Insights.GetSharedKeysArgs
             {
                 ResourceGroupName = items.Item1,
-                WorkspaceName = items.Item2
+                WorkspaceName     = items.Item2
             }));
-
+    
+    // TODO remove this comment
+    
     var managedEnvironment = new ManagedEnvironment(
         environment,
         new ManagedEnvironmentArgs
         {
-            EnvironmentName = environment,
+            EnvironmentName   = environment,
             ResourceGroupName = resourceGroup.Name,
-            ZoneRedundant = false,
+            ZoneRedundant     = false,
             AppLogsConfiguration = new AppLogsConfigurationArgs
             {
                 Destination = "log-analytics",
                 LogAnalyticsConfiguration = new LogAnalyticsConfigurationArgs
                 {
                     CustomerId = workspace.CustomerId,
-                    SharedKey = sharedKeys.Apply(r => r.PrimarySharedKey)!
+                    SharedKey  = sharedKeys.Apply(r => r.PrimarySharedKey)!
                 }
             },
             Tags = { { "environment", environment } }
@@ -69,9 +71,8 @@ return await Pulumi.Deployment.RunAsync(() =>
 
     return new Dictionary<string, object?>
     {
-        ["mgd-env-id"] = managedEnvironment.Id,
-        ["mgd-env-name"] = managedEnvironment.Name,
-        ["mgd-env-ip"] = managedEnvironment.StaticIp,
-        ["aca-rg-name"] = resourceGroup.Name
+        ["Id"]        = managedEnvironment.Id,
+        ["Name"]      = managedEnvironment.Name,
+        ["StaticIp "] = managedEnvironment.StaticIp
     };
 });
--- cloud-resources/src/Azure.ContainerRegistry/Program.cs
+++ cloud-resources/src/Azure.ContainerRegistry/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using Pulumi.AzureNative.Resources;
 using System.Collections.Generic;
 using Pulumi;
-using Pulumi.AzureNative.ContainerRegistry;
-using Pulumi.AzureNative.Resources;
 using Config = Pulumi.Config;
 using AzureNative = Pulumi.AzureNative;
 
@@ -10,14 +9,14 @@ return await Pulumi.Deployment.RunAsync(() =>
 {
     var config = new Config("azure-native");
     var location = config.Require("location");
-
+    
     var resourceGroup = new ResourceGroup("resourceGroup", new ResourceGroupArgs
     {
         ResourceGroupName = "rg-pier8-production",
-        Location = location
+        Location = location,
     });
-
-    var containerRegistry = new Registry("registry", new RegistryArgs
+    
+    var containerRegistry = new AzureNative.ContainerRegistry.Registry("registry", new()
     {
         AdminUserEnabled = true,
         AnonymousPullEnabled = false,
@@ -26,32 +25,19 @@ return await Pulumi.Deployment.RunAsync(() =>
         Location = resourceGroup.Location,
         Sku = new AzureNative.ContainerRegistry.Inputs.SkuArgs
         {
-            Name = SkuName.Basic
-        }
+            Name = AzureNative.ContainerRegistry.SkuName.Basic,
+        },
     }, new CustomResourceOptions
     {
         CustomTimeouts = new CustomTimeouts
         {
-            Create = TimeSpan.FromMinutes(5)
+            Create = TimeSpan.FromMinutes(20) // Set create timeout to 20 minutes
         }
     });
     
-    var credentials = Output.Tuple(resourceGroup.Name, containerRegistry.Name).Apply(items =>
-        ListRegistryCredentials.InvokeAsync(new ListRegistryCredentialsArgs
-        {
-            ResourceGroupName = items.Item1,
-            RegistryName = items.Item2
-        }));
-    
-    var adminUsername = credentials.Apply(c => c.Username);
-    var adminPassword = credentials.Apply(c => c.Passwords[0].Value);
-
     return new Dictionary<string, object?>
     {
-        ["rg-name"] = resourceGroup.Name,
-        ["registry"] = containerRegistry.Name,
-        ["registry-login-server"] = containerRegistry.LoginServer,
-        ["registry-admin-uname"] = adminUsername,
-        ["registry-admin-pwd"] = adminPassword,
+        ["rg-name"]  = resourceGroup.Name,
+        ["registry"] = containerRegistry.Name
     };
 });
